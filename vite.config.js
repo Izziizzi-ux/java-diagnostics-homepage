fd6247bf60079ed2b0b189c7805bae77d440f3b6
Izziizzi-ux <isobel.adcock@ibm.com>
2026-06-18 11:26:33 +0100

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/java-diagnostics-homepage/',
   css: {
     preprocessorOptions: {
       scss: {
